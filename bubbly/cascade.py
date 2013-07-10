d9f02d6d8ff2b88bf8db12081933598b27975d5b
Chris Beaumont <beaumont@hawaii.edu>
2013-07-10 11:05:23 -0400

    Cascaded Booster

---
+++ bubbly/cascade.py
@@ -0,0 +1,264 @@
+from math import ceil
+from warnings import warn
+from functools import wraps
+
+import numpy as np
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import gradient_boosting as gb
+from sklearn.base import clone
+
+
+def _recall_bias(df, frac):
+    """Choose a bias for a decision function, such that
+    at least `frac` fraction of positive examples satisfy
+
+    clf.decision_function(X) >= bias
+
+    Parameters
+    ----------
+    df: array-like
+        Decision function evalued for positive examples
+    frac : float
+        Minimum fraction of positive examples to pass through
+
+    Returns
+    -------
+    float
+    """
+    df.sort()
+
+    tiny = 1e-7
+    if frac <= 0:
+        return df[-1] + tiny
+    if frac >= 1:
+        return df[0]
+
+    ind = int((1 - frac) * df.size)
+    return df[ind]
+
+
+def _fpos_bias(df, fpos, tneg):
+    """Choose a bias for a decision function, such that
+    at most `frac` fraction of negative examples satisfy
+
+    clf.decision_function(X) >= bias
+
+    Parameters
+    ----------
+    df: array-like
+        Decision function evalued for negative examples
+    fpos : float
+        Maximum false positive fraction
+    tneg : int
+        Total number of negative examples (may be larger than df,
+        if previous negatives have been removed in previous versions
+        of the cascade)
+
+    Returns
+    -------
+    float
+    """
+    df.sort()
+
+    tiny = 1e-7
+    fpos = fpos * tneg / df.size
+
+    if fpos >= 1:
+        return df[0] - tiny
+
+    ind = max(min(int((1 - fpos) * df.size), df.size - 1), 0)
+
+    result = df[ind] + tiny
+    assert (df >= result).mean() <= fpos
+
+    return result
+
+
+def _set_bias(clf, X, Y, recall, fpos, tneg):
+    """Choose a bias for a classifier such that the classification
+    rule
+
+    clf.decision_function(X) - bias >= 0
+
+    has a recall of at least `recall`, and (if possible) a false positive rate
+    of at most `fpos`
+
+    Paramters
+    ---------
+    clf : Classifier
+        classifier to use
+    X : array-like [M-examples x N-dimension]
+        feature vectors
+    Y : array [M-exmaples]
+        Binary classification
+    recall : float
+        Minimum fractional recall
+    fpos : float
+        Desired Maximum fractional false positive rate
+    tneg : int
+        Total number of negative examples (including previously-filtered
+        examples)
+    """
+    df = clf.decision_function(X).ravel()
+    r = _recall_bias(df[Y==1], recall)
+    f = _fpos_bias(df[Y==1], fpos, tneg)
+    return min(r, f)
+
+
+def needs_fit(func):
+
+    @wraps(func)
+    def result(self, *args, **kwargs):
+        if not hasattr(self, 'estimators_'):
+            raise ValueError("Estimator not fitted, call `fit` "
+                             "before making predictions")
+        return func(self, *args, **kwargs)
+
+    return result
+
+
+class CascadedBooster(BaseEstimator, ClassifierMixin):
+    def __init__(self, layer_recall=0.99, false_pos=1e-3,
+                 max_layers=5, verbose=0,
+                 base_clf=None):
+        """ A cascaded boosting classifier
+
+        Based on the Viola and Jones (2001) Cascade
+
+        A cascaded classifier aggregates multiple classifiers in
+        sequence. Objects are classified as +1 only if every
+        individual classifier classifies them as +1.
+
+        Parameters
+        ----------
+        layer_recall : float
+            The minimum recall of training data at each layer
+            of the cascade
+        false_pos : float
+            The desired false positive rate of the entire cascade
+        mas_layers : int
+            The maximum number of cascade layers to build during `fit`
+        verbose : int
+            Set verbose > 0 to print informational messages
+        base_clf : Classifier instance (optional)
+            The classifier object to clone and train at each layer of the
+            cascade. Defaults to sklearn.ensemble.GradientBoostingClassifier()
+        """
+
+        if base_clf is None:
+            base_clf = GradientBoostingClassifier()
+
+        self.base_clf = base_clf
+
+        self.layer_recall = layer_recall
+        self.false_pos = false_pos
+        self.max_layers = max_layers
+
+        self.converged = False
+
+        self.verbose = verbose
+
+    @needs_fit
+    def staged_predict(self, X):
+        """Predict classification of `X` for each iteration"""
+        result = np.ones(X.shape[0], dtype=np.int)
+        for b, c in zip(self.bias, self.estimators_):
+            result &= (c.decision_function(X).ravel() >= b)
+            yield result.copy()
+
+    @needs_fit
+    def staged_decision_function(self, X):
+        """Compute decision function of `X` for each iteration"""
+        result = np.zeros(X.shape[0])
+        for c, b in zip(self.estimators_, self.bias):
+            good = result >= 0
+            result[good] = c.decision_function(X[good]) - b
+            yield result.copy()
+
+    @needs_fit
+    def decision_function(self, X):
+        """Compute the decision function of `X`"""
+        result = np.zeros(X.shape[0])
+        for c, b in zip(self.estimators_, self.bias):
+            good = result >= 0
+            result[good] = c.decision_function(X[good]) - b
+        return result
+
+
+    def fit(self, X, Y):
+        """ Fit the casecaded boosting model """
+
+        X, Y = X.copy(), Y.copy()
+        nex = Y.size
+
+        tpos = Y.sum()
+        tneg = nex - tpos
+
+        F = [1.0]
+        self.bias = []
+        self.estimators_ = []
+
+        for i in range(self.max_layers):
+            #assert self._check_invariants(X, Y, tpos, tneg, i)
+
+            if F[-1] < self.false_pos:
+                self.converged = True
+                break
+
+            if np.unique(Y).size == 1:  #down to a single class
+                self.converged = True
+                break
+
+            F.append(F[-1])
+            clf = clone(self.base_clf)
+            clf.fit(X, Y)
+
+            bias = _set_bias(clf, X, Y, self.layer_recall,
+                             self.false_pos, tneg)
+
+            self.estimators_.append(clf)
+            self.bias.append(bias)
+
+            Yp = clf.decision_function(X).ravel() >= bias
+            F[-1] = 1.0 * ((Y == 0) & Yp).sum() / tneg
+            rc = 1.0 * ((Y == 1) & Yp).sum() / tpos
+            if self.verbose > 0:
+                print ("Cascade round %i. False pos rate: %e. "
+                       "Recall: %e" % (i + 1, F[-1], rc))
+
+            if Yp.all():
+                warn("Could not filter any more examples. "
+                     "False positive rate: %e. Recall: %e" % (F[-1], rc))
+                self.converged = False
+                break
+
+            X = X[Yp]
+            Y = Y[Yp]
+
+        else:
+            warn("Could not reduce false positive enough after "
+                 "%i layers. False positive rate: %e. Recall: %e" %
+                 (self.max_layers, F[-1], rc))
+            self.converged = False
+
+        return self
+
+    def add_cascade_layer(self, X, Y):
+        """Add another layer to the cascade.
+
+        The new layer will achieve a recall of at least `layer_recall`
+        on the input data. It will not apply any false positive criteria
+        """
+        clf = clone(self.base_clf)
+        clf.fit(X, Y)
+        bias = _recall_bias(clf.decision_function(X[Y==1]).ravel(),
+                            self.layer_recall)
+        self.estimators_.append(clf)
+        self.bias.append(bias)
+
+    @needs_fit
+    def predict(self, X):
+        """Predict class for X"""
+        return self.decision_function(X).ravel() >= 0
---
+++ bubbly/test/test_cascade.py
@@ -0,0 +1,149 @@
+from sklearn.datasets import make_moons
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import recall_score
+import numpy as np
+import pytest
+
+
+from ..cascade import CascadedBooster, _recall_bias, _fpos_bias
+from ..util import false_pos
+
+def test_recall_bias():
+
+    df = np.arange(5)
+
+    for thresh in [0, .1, .2, .21, .5, 1]:
+        bias = _recall_bias(df, thresh)
+        assert (df >= bias).mean() >= thresh
+
+def test_fpos_bias():
+    df = np.arange(5)
+
+    for thresh in [0, .1, .2, .21, .5, 1]:
+        bias = _fpos_bias(df, thresh, 5)
+        assert (df >= bias).mean() <= thresh
+
+
+class TestCascade(object):
+
+    def setup_method(self, method):
+        base = GradientBoostingClassifier(n_estimators=2)
+        self.clf = CascadedBooster(base_clf=base)
+
+        n_samples = 500
+        np.random.seed(42)
+        X, Y = make_moons(n_samples=n_samples, noise=.05)
+
+        self.X = X
+        self.Y = Y
+        self.clf.fit(X, Y)
+
+    def teardown_method(self, method):
+        self.check_clf(self.clf, self.X, self.Y)
+
+    def test_multi_layer(self):
+        assert len(self.clf.estimators_) > 1
+
+    def check_recall_constraints(self, clf, x, y):
+        #recall is at least as good as required
+        recall_i = clf.layer_recall
+        for i, yp in enumerate(clf.staged_predict(x), 1):
+            assert recall_score(y, yp) >= (recall_i ** i)
+        np.testing.assert_array_equal(yp, clf.predict(x))
+
+    def check_fpos_constraints(self, clf, x, y):
+        # if classifier converged, false pos rate on training data <= false_pos
+        if clf.converged:
+            assert false_pos(y, clf.predict(x)) <= clf.false_pos
+
+    def check_staged_decision(self, clf, x, y):
+        for i, yp in enumerate(clf.staged_decision_function(x)):
+            good = yp > 0
+            #df for positive examples == df for last stage
+            expect = clf.estimators_[i].decision_function(x[good]) - clf.bias[i]
+            np.testing.assert_array_equal(yp[good].ravel(), expect.ravel())
+
+        np.testing.assert_array_equal(clf.decision_function(x), yp)
+
+    def check_staged_predict(self, clf, x, y):
+        #staged predict and decision function are consistent
+        from itertools import izip
+        for df, yp in izip(clf.staged_decision_function(x),
+                           clf.staged_predict(x)):
+            np.testing.assert_array_equal((df >= 0).ravel(), (yp > 0).ravel())
+
+    def check_clf(self, clf, x, y):
+        self.check_recall_constraints(clf, x, y)
+        self.check_fpos_constraints(clf, x, y)
+        self.check_staged_decision(clf, x, y)
+        self.check_staged_predict(clf, x, y)
+
+    def test_recall_stages(self):
+        self.check_recall_constraints(self.clf, self.X, self.Y)
+
+        self.clf.layer_recall = 1.0
+        self.clf.fit(self.X, self.Y)
+        self.check_recall_constraints(self.clf, self.X, self.Y)
+        assert recall_score(self.clf.predict(self.X), self.Y) == 1
+
+    def test_fit_converge(self):
+        self.clf.false_pos = 0.5
+        self.clf.fit(self.X, self.Y)
+        assert self.clf.converged
+
+    def test_fit_maxiter(self):
+        self.clf.false_pos = 0
+        self.clf.layer_recall = .6
+
+        self.clf.max_layers = 4
+
+        np.random.shuffle(self.Y)
+        self.clf.fit(self.X, self.Y)
+
+        assert not self.clf.converged
+        assert len(self.clf.bias) == 4
+
+    def test_impossible_filter(self):
+        self.clf.layer_recall = 1
+        self.clf.false_pos = 0
+        np.random.shuffle(self.Y)
+
+        self.clf.fit(self.X, self.Y)
+        assert not self.clf.converged
+        assert len(self.clf.bias) < 4
+
+    def test_separable_fit(self):
+        self.clf.layer_recall = .5
+        self.clf.false_pos = .5
+
+        self.X = np.arange(10).reshape(-1, 1)
+        self.Y = (self.X.ravel() > 5).astype(np.int)
+
+        self.clf.fit(self.X, self.Y)
+        assert self.clf.converged
+        assert len(self.clf.estimators_) == 1
+
+        assert recall_score(self.Y, self.clf.predict(self.X)) == 1
+        assert false_pos(self.Y, self.clf.predict(self.X)) == 0
+
+    def test_add_layer(self):
+        recall = recall_score(self.Y, self.clf.predict(self.X))
+        fpos = false_pos(self.Y, self.clf.predict(self.X))
+        n_layers = len(self.clf.estimators_)
+
+        self.clf.add_cascade_layer(self.X, self.Y)
+        assert len(self.clf.estimators_) == n_layers + 1
+        yp = self.clf.predict(self.X)
+        assert false_pos(self.Y, yp) <= fpos
+        assert recall_score(self.Y, yp) >= recall * self.clf.layer_recall
+
+    def test_predict_before_fit(self):
+
+        clf = CascadedBooster()
+        for func in [clf.staged_predict,
+                     clf.staged_decision_function,
+                     clf.predict,
+                     clf.decision_function,
+                     ]:
+            with pytest.raises(ValueError) as exc:
+                func()
