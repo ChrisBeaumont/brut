65d746e9888f75253c15662cfe9d3c23739771cd
Chris Beaumont <beaumont@hawaii.edu>
2013-07-17 17:51:45 -0400

    more model methods

--- bubbly/model.py
+++ bubbly/model.py
@@ -2,6 +2,7 @@ from itertools import islice, ifilter
 import warnings
 import logging
 import cPickle as pickle
+import json
 
 logging.getLogger(__name__).setLevel(logging.DEBUG)
 
@@ -11,6 +12,8 @@ import cloud
 
 from .cascade import CascadedBooster
 from .decorators import profile
+from .util import chunk
+
 
 class Model(object):
 
@@ -60,6 +63,15 @@ class Model(object):
         with open(path) as infile:
             return pickle.load(infile)
 
+    def save_training_data(self, path):
+        with open(path, 'w') as outfile:
+            json.dump(self.training_data, outfile)
+
+    @staticmethod
+    def load_training_data(path):
+        with open(path) as infile:
+            return json.load(infile)
+
     def predict(self, params):
         if not hasattr(params[0], '__len__'):
             params = [params]
@@ -83,12 +95,70 @@ class Model(object):
         return ifilter(pos, self.locator.negatives_iterator())
 
     @profile
-    def cloud_false_positives(self, num, workers=10):
-        logging.getLogger(__name__).debug("Cloud scanning for %i false "
-                                          "positives" % num)
-        jobs = cloud.map(self.false_positives, [num / workers] * workers,
-                         _env='mwp', _type='c2')
+    def cloud_false_positives(self, num=0, workers=10, jobs=None):
+        """
+        Use PiCloud to find false positives
+
+        Usage:
+
+        cloud_false_positives(15)
+
+        or
+
+        cloud_false_positives(jobs=range(15, 20))
+
+        Parameters
+        ----------
+        num : int
+            number of false positives to search for
+        workers : int (optional, default=10)
+            number of PiCloud jobs to use
+        jobs : sequence of ints (optional)
+            If provided, re-fetch a previous patch
+            a previous batch of PiCloud jobs
+        """
+        if jobs is None:
+            logging.getLogger(__name__).debug("Scanning for %i false "
+                                              "positives on PiCloud" % num)
+            jobs = cloud.map(self.false_positives, [num / workers] * workers,
+                             _env='mwp', _type='c2')
+            logging.getLogger(__name__).info(
+                "To re-fetch results, use \n"
+                "cloud_false_positives(jobs=range(%i, %i))",
+                min(jobs), max(jobs) + 1)
         return [r for j in cloud.result(jobs) for r in j]
+
+    def cloud_decision_function(self, x, workers=10, jobs=None):
+        if jobs is None:
+            logging.getLogger(__name__).debug("Classifying %i features "
+                                              "on PiCloud" % len(x))
+            jobs = cloud.map(self.decision_function, chunk(x, workers),
+                             _env='mwp', _type='c2')
+            logging.getLogger(__name__).info(
+                "To re-fetch results, use \n"
+                "cloud_decision_function(jobs=range(%i, %i))",
+                min(jobs), max(jobs) + 1)
+        return np.hstack(cloud.result(jobs))
+
+    def decision_function(self, x):
+        """
+        Compute the decision function for a list of stamp descriptions
+
+        Parameters
+        ----------
+        x : List of stamp description tuples
+
+        Returns
+        -------
+        An ndarray of the decision function for each feature extracted
+        from x
+        """
+        result = np.empty(len(x))
+        for i, ex in enumerate(x):
+            X, _ = self._make_x_y([ex], None).reshape(1, -1)
+            df = self.estimator.decision_function(X).ravel()
+            result[i] = df
+        return result
 
     def _reset(self):
         self.training_data = []
