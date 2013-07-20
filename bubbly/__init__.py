31e1b6183e93df7e44e8153d4b4f2120418ae17e
Chris Beaumont <beaumont@hawaii.edu>
2013-07-20 14:34:32 -0400

    try verbose logging

--- bubbly/__init__.py
+++ bubbly/__init__.py
@@ -1,2 +1,3 @@
 import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+logging.getLogger(__name__).addHandler(logging.StreamHandler())
