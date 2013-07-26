e318b53bbdf523d3e6363320b5cc9e8e45623957
Chris Beaumont <beaumont@hawaii.edu>
2013-07-26 15:07:49 -0400

    new figures

---
+++ figures/roc_all.py
@@ -0,0 +1,33 @@
+import json
+
+from sklearn.metrics import roc_curve
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    labels = json.load(open('../models/benchmark_scores.json'))
+    off = labels['off_score']
+
+    on = json.load(open('../models/bubble_scores.json'))['scores']
+
+    yp = np.array(on + off)
+    y = np.array([1] * len(on) + [0] * len(off))
+
+    pars = np.array(labels['on'] + labels['off'])
+    for i in [0, 1, 2]:
+        mask = pars[:, 0] % 3 == i
+        fp, tp, _ = roc_curve(y[mask], yp[mask])
+        plt.plot(fp, tp, label = 'Model %i' % (i + 1))
+
+
+    plt.xlim(0, .002)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend(loc='lower right')
+    plt.gcf().set_tight_layout(True)
+    plt.savefig('roc_all.eps')
+
+if __name__ == "__main__":
+    main()
