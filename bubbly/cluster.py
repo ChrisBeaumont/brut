bb28cfa76b1f5028aedad6293e772bdb8e029544
Chris Beaumont <beaumont@hawaii.edu>
2013-08-01 12:24:52 -0400

    simplify clustering approach

--- bubbly/cluster.py
+++ bubbly/cluster.py
@@ -18,78 +18,12 @@ def distance(s, inds):
 
     return result
 
-def _merge_rows(stamps, scores, i, j):
-    if scores[i] > scores[j]:
-        return np.delete(stamps, j, 0)
-    else:
-        return np.delete(stamps, i, 0)
+def _merge(result, dist, i, j, scores):
+    kill = i if scores[i] < scores[j] else j
+    result[kill] = False
 
-def nn_similar_size(stamps, size_similarity=2.0):
-    """
-    Given a stamp array, return the index of the nearest neighbor
-    to each point, only considering neighbors of similar size
-
-    Parameters
-    ----------
-    stamps : ndarray
-        stamps array as described in `merge`
-
-    size_similarity : float, optional
-        Relative size similarity threshold. Neighbors differ in size
-        by less than this fraction
-
-    Returns
-    -------
-    integer array, giving the neighbor index of each row in stamps.
-    Rows with no neighbor are assigned -1
-    """
-    xy = stamps[:, 1:3]
-    r = stamps[:, 3]
-    tree = NearestNeighbors()
-    tree.fit(xy)
-
-    result = np.zeros(stamps.shape[0], dtype=np.int) - 1
-
-    for i in range(stamps.shape[0]):
-        k = 5
-        while True:
-            ds, ids = tree.kneighbors(xy[i], k)
-
-            ds = ds.ravel()
-            ids = ids.ravel()
-
-            other = ids != i
-            ds = ds[other]
-            ids = ids[other]
-
-            if ds.size == 0:
-                break
-
-            ds /= np.sqrt(r[i] * r[ids])
-            dmax = ds.max()
-
-            size_ratio = r[i] / r[ids]
-            size_ratio = np.maximum(size_ratio, 1 / size_ratio)
-            good = size_ratio <= size_similarity
-            good &= (ds < 1)
-
-            ds = ds[good]
-            ids = ids[good]
-
-            if ds.size > 0:
-                result[i] = ids[0]
-                break
-
-            if dmax >= 1:
-                break
-
-            if k >= stamps.shape[0]:
-                break
-
-            k = min(k * 2, stamps.shape[0])
-
-    return result
-
+    dist[kill, :] = np.inf
+    dist[:, kill] = np.inf
 
 def merge(stamps, scores):
     """
@@ -107,20 +41,41 @@ def merge(stamps, scores):
     -------
     A row-subset of stamps, containing the unique objects
     """
-
     stamps = np.atleast_2d(stamps)
-    while True:
-        if stamps.shape[0] == 1:
-            return stamps
 
-        nearest = nn_similar_size(stamps)
-        if nearest.max() == -1:
-            return stamps
-        d = distance(stamps, nearest)
+    d = bubble_distance_matrix(stamps, stamps)
+
+    eye = np.arange(d.shape[0])
+    d[eye, eye] = np.inf
+
+    result = np.ones(d.shape[0], dtype=np.bool)
+
+    while np.isfinite(d).any():
         best = np.argmin(d)
-        i, j = best, nearest[best]
+        i, j = np.unravel_index(best, d.shape)
+        _merge(result, d, i, j, scores)
 
-        stamps = _merge_rows(stamps, scores, i, j)
+    return stamps[result]
+
+
+def bubble_distance_matrix(a, b):
+
+    d = np.hypot(a[:, 1].reshape(-1, 1) - b[:, 1].reshape(1, -1),
+                 a[:, 2].reshape(-1, 1) - b[:, 2].reshape(1, -1))
+
+    ra = a[:, -1].reshape(-1, 1)
+    rb = b[:, -1].reshape(1, -1)
+    size = np.minimum(ra, rb)
+
+    d /= size
+
+    bad = d >= 1
+    d[bad] = np.inf
+
+    ratio = np.maximum(ra / rb, rb / ra)
+    bad = ratio > 2
+    d[bad] = np.inf
+    return d
 
 
 def xmatch(a, b):
@@ -148,18 +103,7 @@ def xmatch(a, b):
 
     a = np.asarray(a)
     b = np.asarray(b)
-    #distance matrix
-    d = np.hypot(a[:, 1].reshape(-1, 1) - b[:, 1].reshape(1, -1),
-                 a[:, 2].reshape(-1, 1) - b[:, 2].reshape(1, -1))
-
-    ra = a[:, -1].reshape(-1, 1)
-    rb = b[:, -1].reshape(1, -1)
-    bad = (d > np.minimum(ra, rb))
-    d[bad] = np.inf
-
-    ratio = np.maximum(ra / rb, rb / ra)
-    bad = ratio > 2
-    d[bad] = np.inf
+    d = bubble_distance_matrix(a, b)
 
     result = np.zeros(a.shape[0], dtype=np.int) - 1
 
