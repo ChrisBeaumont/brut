f6ad0d612537d54132febc49e121c79a433d41d9
Chris Beaumont <beaumont@hawaii.edu>
2013-08-15 11:31:50 -0400

    Get rid of bubble index

--- scripts/bubble_classify.py
+++ scripts/bubble_classify.py
@@ -6,19 +6,13 @@ from bubbly.dr1 import bubble_params
 def main():
 
     model = ModelGroup.load('../models/full_classifier.dat')
-    ind = np.argsort(bubble_params())
     bubbles = sorted(bubble_params())
     scores = model.decision_function(bubbles)
 
-
-
-    result = {'params': bubbles, 'scores': scores.tolist(),
-              'ind': ind}
+    result = {'params': bubbles, 'scores': scores.tolist()}
 
     with open('../models/bubble_scores.json', 'w') as outfile:
         json.dump(result, outfile)
-
-
 
 
 if __name__ == "__main__":
